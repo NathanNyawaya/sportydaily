89144fb06f80ebd569874d99f86108f8e1d40dde
NathanNyawaya <nyawayanathan@gmail.com>
2023-11-01 08:18:54 +0300

    remove top website add

--- src/app/components/ads/SideBarAds.jsx
+++ src/app/components/ads/SideBarAds.jsx
@@ -9,11 +9,7 @@ const SideBarAds = () => {
         <p className="opacity-80 text-sm">Ads</p>
         <div className="relative pt-10">
           {/* <CloseIcon className="cursor-pointer text-white absolute top-0 right-0 w-[24px] h-[24px] rounded-full border p-1" /> */}
-          {/* <img
-            src="https://images.unsplash.com/photo-1517747614396-d21a78b850e8?auto=format&fit=crop&q=80&w=2127&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="ads"
-            className="rounded border"
-          /> */}
+          
 
           <a target="_blank" href="https://beta.publishers.adsterra.com/referral/RRkKmkFg3T">
             <img

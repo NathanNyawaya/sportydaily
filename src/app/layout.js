856110a61cb675d7dc660a3738ff0e7d81938531
NathanNyawaya <nyawayanathan@gmail.com>
2023-11-05 15:39:03 +0300

    analytics powered by google added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import GoogleAnalytics from "./analytics/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID ? (
+          <GoogleAnalytics
+            ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}
+          />
+        ) : null}
+        {children}
+      </body>
     </html>
   );
 }

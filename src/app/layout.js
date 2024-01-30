d5e1b452fac1ed3703dd7b3680b9201cad3d6665
NathanNyawaya <nyawayanathan@gmail.com>
2024-01-30 03:58:16 +0300

    added clarity ms

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ktoq0i85n9");
+            `,
+          }}
+        />
+      </Head>
       <body className={`${roboto.variable}`}>
         {process.env.NEXT_PUBLIC_PROD ? (
           <GoogleAnalytics
